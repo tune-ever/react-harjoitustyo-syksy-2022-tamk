1c7f88dab10a160151d0516cd0c04fd2b74be656
tune-ever <andreas.achte@gmail.com>
2022-11-17 14:31:47 +0200

    React-router simple skeleton

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React from "react";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import Tasks from "./components/Tasks.js";
+import Info from "./components/Info.js";
+import Faq from "./components/Faq.js";
 
 function App() {
+  const navStyle = {
+    padding: 5
+  };
   return (
-    <div className="App">
-      <p>Hello World</p>
-    </div>
+    <BrowserRouter>
+      <nav>
+        <Link style={navStyle} to="/">
+          Tasks
+        </Link>
+        <Link style={navStyle} to="/info">
+          Info
+        </Link>
+        <Link style={navStyle} to="/faq">
+          Faq
+        </Link>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Tasks />} />
+        <Route path="/info" element={<Info />} />
+        <Route path="/faq" element={<Faq />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
