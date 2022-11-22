863a9c48a16a206c7836c65e227f6613cc2410a8
tune-ever <andreas.achte@gmail.com>
2022-11-22 13:52:51 +0200

    Added instructions for using the site

--- src/components/Info.js
+++ src/components/Info.js
@@ -3,7 +3,7 @@ const Info = () => {
     <div>
       <h2>Info page</h2>
       <article>Name: Andreas Achte</article>
-      <p>Here will be the instructions for this site.</p>
+      <p>Currently no instructions is needed for this site.</p>
       <p>No copyright material have been used on this site.</p>
     </div>
   );
