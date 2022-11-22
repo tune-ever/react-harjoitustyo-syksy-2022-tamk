3136f049c986e2237d872211d41d55576d91314f
tune-ever <andreas.achte@gmail.com>
2022-11-22 13:54:27 +0200

    Faq.js: renamed to FreeTime.js

--- src/components/Faq.js
+++
@@ -1,5 +0,0 @@
-const Faq = () => {
-  return <p>Faq</p>;
-};
-
-export default Faq;
---
+++ src/components/FreeTime.js
@@ -0,0 +1,20 @@
+const FreeTime = () => {
+  return (
+    <div>
+      <article>
+        Here is some activies suggestions if you have no active tasks
+      </article>
+      <ul>
+        <li>Eat candy</li>
+        <li>Visit a friend</li>
+        <li>Drink tea</li>
+        <li>Watch sports</li>
+        <li>Go for a walk</li>
+        <li>Watch a movie</li>
+        <li>Call your mom</li>
+      </ul>
+    </div>
+  );
+};
+
+export default FreeTime;
