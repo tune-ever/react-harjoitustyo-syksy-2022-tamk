ded024c96783088d68d502b11a61a1314892451b
tune-ever <andreas.achte@gmail.com>
2022-11-22 13:53:46 +0200

    Task.js: Added header for contexts

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,11 +1,11 @@
 const Task = props => {
   const name = props.task.name;
   const contexts = props.task.contexts;
-
   return (
     <div>
       {name}
       <section>
+        <p>Contexts:</p>
         <ul>{contexts.map(context => <li key={context}>{context}</li>)}</ul>
       </section>
     </div>
