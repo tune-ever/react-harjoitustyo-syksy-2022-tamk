3d1995347c055a0eff73519c7d8cb82899ab3a48
tune-ever <andreas.achte@gmail.com>
2022-11-25 14:58:43 +0200

    Tasks.js: useEffect(): calls taskService.getAll. ChangeName(): updates a single task's name in state. AddContext(): adds a new context to a task stored in state. Render <Task>: ChangeName and AddContext are passed as props down to <Task> component.

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,13 +1,51 @@
 import { useState, useEffect } from "react";
-import axios from "axios";
 import Task from "./Task.js";
+import taskService from "../services/taskService";
 
 const Tasks = () => {
   const [tasks, setTasks] = useState([]);
+  console.log(tasks);
 
+  // First page load -> get tasks from db.json
   useEffect(() => {
-    axios.get("http://localhost:3010/tasks").then(res => setTasks(res.data));
+    taskService.getAll().then(res => setTasks(res.data));
   }, []);
+
+  // Function to change the name of a task
+  const changeName = (id, newName) => {
+    const newTasksArray = [];
+
+    // Iterate the array -> replace the match with new name
+    tasks.map(task => {
+      // Found matching id?
+      if (task.id === id) {
+        // Push updated task
+        const newTask = {
+          id: id,
+          name: newName,
+          contexts: task.contexts
+        };
+        newTasksArray.push(newTask);
+      } else {
+        newTasksArray.push(task);
+      }
+    });
+    setTasks(newTasksArray);
+  };
+
+  // Function to add a context
+  const addContext = (id, newContext) => {
+    const newTasksArray = [];
+    // Iterate tasks, find match, add the new context
+    tasks.map(task => {
+      if (task.id === id) {
+        task.contexts.push(newContext);
+      }
+      newTasksArray.push(task);
+    });
+    setTasks(newTasksArray);
+  };
+
   return (
     <div>
       <h2>Tasks</h2>
@@ -16,7 +54,13 @@ const Tasks = () => {
         <ol>
           {tasks.map(task => (
             <li key={task.id}>
-              <Task task={task} key={task.id} />
+              {/* Props: function addContext, function changeName, task object, key*/}
+              <Task
+                addContext={addContext}
+                changeName={changeName}
+                task={task}
+                key={task.id}
+              />
             </li>
           ))}
         </ol>
