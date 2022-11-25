a6fe6bafbb5c0b5c748c4e977a95eba8a9c5ea42
tune-ever <andreas.achte@gmail.com>
2022-11-25 17:44:44 +0200

    AddTask.js: handleTaskSubmit(): function to submit the user's input to parent element. Input is: name(string), max three contexts(string). Added one more text input for the user. All form element values are stored in state.

--- src/components/AddTask.js
+++ src/components/AddTask.js
@@ -1,9 +1,32 @@
 import { useState } from "react";
 
+// This component is a simple form to add a new task
 const AddTask = props => {
   const [nameInput, setNameInput] = useState([]);
+  const [contextInput, setContextInput] = useState([]);
+  const [contextInputTwo, setContextInputTwo] = useState([]);
+  const [contextInputThree, setContextInputThree] = useState([]);
 
-  const handleTaskSubmit = () => {};
+  const handleTaskSubmit = event => {
+    // This prevents default refresh
+    event.preventDefault();
+    const contexts = [];
+    contexts.push(contextInput);
+    contexts.push(contextInputTwo);
+    contexts.push(contextInputThree);
+
+    // Name is required: a check
+    if (nameInput) {
+      // Call parent element's function addTask()
+      props.addTask(nameInput, contexts);
+    }
+
+    // Set the inputs to empty after submit.
+    setNameInput("");
+    setContextInput("");
+    setContextInputTwo("");
+    setContextInputThree("");
+  };
 
   return (
     <form type="submit" onSubmit={handleTaskSubmit}>
@@ -17,15 +40,22 @@ const AddTask = props => {
       <input
         placeholder="Context name"
         type="text"
-        value={nameInput}
-        onChange={e => setNameInput(e.target.value)}
+        value={contextInput}
+        onChange={e => setContextInput(e.target.value)}
       />
       <br />
       <input
         placeholder="Context name"
         type="text"
-        value={nameInput}
-        onChange={e => setNameInput(e.target.value)}
+        value={contextInputTwo}
+        onChange={e => setContextInputTwo(e.target.value)}
+      />
+      <br />
+      <input
+        placeholder="Context name"
+        type="text"
+        value={contextInputThree}
+        onChange={e => setContextInputThree(e.target.value)}
       />
       <br />
       <input type="submit" />
