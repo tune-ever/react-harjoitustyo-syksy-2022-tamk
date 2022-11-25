1a19bee494d4b2ee8db553e3132f0c91c4e31bd7
tune-ever <andreas.achte@gmail.com>
2022-11-25 14:55:19 +0200

    Task.js: Renders a React form. HandleSubmit: Function handles user's input passed from form. Calls parent component's function changeName. Text input's value is stored in state.

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,9 +1,35 @@
+import { useState } from "react";
+
+// This component has basic react form: value is state!
 const Task = props => {
+  const [nameInput, setNameInput] = useState([]);
+
   const name = props.task.name;
   const contexts = props.task.contexts;
+
+  // this function handles the submit
+  const handleSubmit = event => {
+    // Prevents html refresh
+    event.preventDefault();
+    // Calls the changeName function passed down as props
+    props.changeName(props.task.id, nameInput);
+    // set textbox to empty
+    setNameInput("");
+  };
+
   return (
     <div>
       {name}
+      {/* This is a basic react form for updating task name:uses state etc. */}
+      <form type="submit" onSubmit={handleSubmit}>
+        <input
+          placeholder="update task name"
+          type="text"
+          value={nameInput}
+          onChange={e => setNameInput(e.target.value)}
+        />
+        <input type="submit" />
+      </form>
       <section>
         <p>Contexts:</p>
         <ul>{contexts.map(context => <li key={context}>{context}</li>)}</ul>
