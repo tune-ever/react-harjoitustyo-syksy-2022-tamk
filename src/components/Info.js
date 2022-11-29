1a0774ab4967a1bef9e87acee1f091a36178341c
tune-ever <andreas.achte@gmail.com>
2022-11-29 16:53:30 +0200

    Info.js: Instructions for using the site.

--- src/components/Info.js
+++ src/components/Info.js
@@ -2,9 +2,28 @@ const Info = () => {
   return (
     <div>
       <h2>Info page</h2>
-      <article>Name: Andreas Achte</article>
-      <p>Currently no instructions is needed for this site.</p>
-      <p>No copyright material have been used on this site.</p>
+      <article>Creator: Andreas Achte</article>
+      <br />
+      <article>
+        Tasks page: you can view a list of current tasks. You can add new tasks,
+        rename current tasks and edit the contexts of the tasks.
+      </article>
+      <ul>
+        <li>
+          Rename a task: type to textbox under task name and press submit button
+        </li>
+        <li>Remove a context: press remove button next to context</li>
+        <li>
+          Add a new context: type to textbox under contexts and press submit
+          button
+        </li>
+        <li>Remove a task: press "Remove task" button (bottom of a task.)</li>
+        <li>
+          Add a new task: scroll to the bottom of the page: type task name and
+          contexts, then press submit button
+        </li>
+      </ul>
+      <footer>No copyright material have been used on this site.</footer>
     </div>
   );
 };
