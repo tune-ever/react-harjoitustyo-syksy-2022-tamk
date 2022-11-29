ac950e11040763cd88f67fc857770ea83aebcacf
tune-ever <andreas.achte@gmail.com>
2022-11-29 16:55:10 +0200

    taskService.js: Three rest api functions: updateById(put), addTask(post), deleteTask(delete).

--- src/services/taskService.js
+++ src/services/taskService.js
@@ -7,8 +7,26 @@ const getAll = async () => {
   return response;
 };
 
+// Update the name of a task
+const updateById = async (id, task) => {
+  // Simple put request to db: body is new task
+  axios.put(`${url}/${id}`, task);
+};
+
+const addTask = async task => {
+  const response = await axios.post(url, task);
+  return response;
+};
+
+const deleteTask = async id => {
+  axios.delete(`${url}/${id}`);
+};
+
 const taskService = {
-  getAll
+  getAll,
+  updateById,
+  deleteTask,
+  addTask
 };
 
 export default taskService;
