34046b8fe281d0033d2d8da34488e384491899d9
Andreas Achte <andreas.achte@gmail.com>
2022-12-02 11:06:06 +0200

    Render filter buttons. Filters passed as props. Show all button too.

--- src/components/FilterElement.js
+++ src/components/FilterElement.js
@@ -1,8 +1,25 @@
-import FilterButton from "./FilterButton";
-import { useState } from "react";
-
 const FilterElement = props => {
-  // Contexts:
+  const filters = props.filters;
+
+  // Contexts array from tasks prop:
+  const contextArray = [];
+  // For loops with duplicate chek:
+  props.tasks.forEach(task => {
+    task.contexts.forEach(context => {
+      if (!contextArray.includes(context)) contextArray.push(context);
+    });
+  });
+
+  return (
+    <section>
+      <button onClick={() => props.clearFilters()}>Show all</button>
+      {contextArray.map(context => (
+        <button onClick={() => props.handleFilterClick(context)} key={context}>
+          {context}
+        </button>
+      ))}
+    </section>
+  );
 };
 
 export default FilterElement;
