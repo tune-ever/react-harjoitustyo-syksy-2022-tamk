6465415148d99880ee9843607d3cb01313f9412c
Andreas Achte <andreas.achte@gmail.com>
2022-12-02 14:05:51 +0200

    Render filter buttons for user: one button for each different context. Buttons call parent element's functions to update the filter state.

--- src/components/FilterElement.js
+++ src/components/FilterElement.js
@@ -3,21 +3,36 @@ const FilterElement = props => {
 
   // Contexts array from tasks prop:
   const contextArray = [];
-  // For loops with duplicate chek:
+  // Here we find all the necessary filter buttons:
   props.tasks.forEach(task => {
     task.contexts.forEach(context => {
       if (!contextArray.includes(context)) contextArray.push(context);
     });
   });
 
+  // Render the buttons: also include a show all button: clears all filters:
   return (
     <section>
       <button onClick={() => props.clearFilters()}>Show all</button>
-      {contextArray.map(context => (
-        <button onClick={() => props.handleFilterClick(context)} key={context}>
-          {context}
-        </button>
-      ))}
+      {contextArray.map(context =>
+        // if filter is active now, change style to green:
+        filters.includes(context) ? (
+          <button
+            style={{ backgroundColor: "lightGreen" }}
+            onClick={() => props.handleFilterClick(context)}
+            key={context}
+          >
+            {context}
+          </button>
+        ) : (
+          <button
+            onClick={() => props.handleFilterClick(context)}
+            key={context}
+          >
+            {context}
+          </button>
+        )
+      )}
     </section>
   );
 };
