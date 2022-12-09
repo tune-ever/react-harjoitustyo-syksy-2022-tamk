43592926d5a0034db5eb57a1fbd73339fdfee91c
Andreas Achte <andreas.achte@gmail.com>
2022-12-09 14:45:40 +0200

    New function: Patch: updates status of a task. Some syntax sugar.

--- src/services/taskService.js
+++ src/services/taskService.js
@@ -6,32 +6,41 @@ const url = "http://localhost:3010/tasks";
 
 // Get:
 const getAll = async () => {
-    const response = await axios.get(url);
-    return response;
+  const response = await axios.get(url);
+  return response;
 };
 
 // Put:
 const updateById = async (id, task) => {
-    // Simple put request to db: body is new task
-    axios.put(`${url}/${id}`, task);
+  // Simple put request to db: body is new task
+  axios.put(`${url}/${id}`, task);
 };
 
 // Post:
-const addTask = async (task) => {
-    const response = await axios.post(url, task);
-    return response;
+const addTask = async task => {
+  const response = await axios.post(url, task);
+  return response;
 };
 
 // Delete:
-const deleteTask = async (id) => {
-    axios.delete(`${url}/${id}`);
+const deleteTask = async id => {
+  axios.delete(`${url}/${id}`);
+};
+
+// Patch:
+const changeStatusById = async (id, status) => {
+  const patchObject = {
+    status: status,
+  };
+  axios.patch(`${url}/${id}`, patchObject);
 };
 
 const taskService = {
-    getAll,
-    updateById,
-    deleteTask,
-    addTask,
+  getAll,
+  updateById,
+  deleteTask,
+  addTask,
+  changeStatusById,
 };
 
 export default taskService;
