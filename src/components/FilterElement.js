63127d88a1f0062494cce1b0cd87340dd390f5e3
Andreas Achte <andreas.achte@gmail.com>
2022-12-12 14:36:52 +0200

    Activity of show all button id displayd to user in green.

--- src/components/FilterElement.js
+++ src/components/FilterElement.js
@@ -4,10 +4,20 @@ const FilterElement = props => {
   // Render the buttons: also include a show all button: clears all filters:
   return (
     <section>
-      <button onClick={() => props.clearFilters()}>Show all</button>
+      {/* if all active, show all is green */}
+      {filters.includes("all") ? (
+        <button
+          style={{ backgroundColor: "lightGreen" }}
+          onClick={() => props.clearFilters()}
+        >
+          Show all
+        </button>
+      ) : (
+        <button onClick={() => props.clearFilters()}>Show all</button>
+      )}
       {/* Iterate contexts: */}
       {props.contextArray.map(context =>
-        // if filter is active now, change style to green:
+        // if filter is active now, change style to green: conditional rendering.
         filters.includes(context) ? (
           <button
             style={{ backgroundColor: "lightGreen" }}
