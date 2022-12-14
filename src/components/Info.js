fd99cc76ccf41da257b1aedeb77cd9fa027a4985
Andreas Achte <andreas.achte@gmail.com>
2022-12-14 14:21:35 +0200

    To finnish.

--- src/components/Info.js
+++ src/components/Info.js
@@ -1,30 +1,22 @@
 const Info = () => {
   return (
     <div>
-      <h2>Info page</h2>
-      <article>Creator: Andreas Achte</article>
+      <h2>Info sivu</h2>
+      <article>Tekijä: Andreas Achte</article>
       <br />
-      <article>
-        Tasks page: you can view a list of current tasks. You can add new tasks,
-        rename current tasks and edit the contexts of the tasks.
-      </article>
+      <h4>Tehtävät</h4>
       <ul>
+        <li>Sivu toimii tehtävä listana.</li>
         <li>
-          Rename a task: type to textbox under task name and press submit button
+          Tehtäviä voi lisätä ja poistaa. (ylhäällä tehtävän lisäys, poisto
+          tehtävä elementin alaosan nappia painamalla.)
         </li>
-        <li>Remove a context: press remove button next to context</li>
+        <li>Kullakin tehtävällä voi olla monia eri konteksteja.</li>
         <li>
-          Add a new context: type to textbox under contexts and press submit
-          button
+          Konteksteja voi lisäillä ja poistella napeista: kts. poista-nappi sekä
+          "lisää konteksti" -syöte.
         </li>
-        <li>Remove a task: press "Remove task" button (bottom of a task.)</li>
-        <li>
-          Add a new task: scroll to the bottom of the page: type task name and
-          contexts, then press submit button
-        </li>
-        <li>
-          Filtering tasks: press filter buttons. Show all button clears filters.
-        </li>
+        <li>Suodattaminen:</li>
         <li>
           Moving items from undone to done: drag and drop with mouse. Move to
           other list.
