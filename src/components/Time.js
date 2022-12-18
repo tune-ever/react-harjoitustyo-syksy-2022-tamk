ce0ed77082e73651c6616182d19c60db55e55f67
Andreas Achte <andreas.achte@gmail.com>
2022-12-18 14:01:23 +0200

    User can list task times: for each task, each context, user can edit task intervals too.

--- src/components/Time.js
+++ src/components/Time.js
@@ -1,4 +1,5 @@
 import DateTimePicker from "react-datetime-picker";
+import TimePicker from "react-time-picker";
 import { useState, useEffect } from "react";
 import taskService from "../services/taskService";
 import {
@@ -7,6 +8,7 @@ import {
   compareAsc,
   compareDesc,
 } from "date-fns";
+import TimePickerButton from "./TimePickerButton.js";
 
 const Time = () => {
   // Set starting time to zero:
@@ -16,7 +18,13 @@ const Time = () => {
   // starttime start of today:
   const [startTime, onChangeStart] = useState(new Date(today));
   const [endTime, onChangeEnd] = useState();
+  const [intervalTimes, onChange] = useState([]);
+
+  // tasks
   const [tasks, setTasks] = useState([]);
+
+  // showInterval Index:
+  const [showIntervalIndex, setShowIntervalIndex] = useState([]);
 
   // Two for loops to find all unique contexts from tasks:
   const contexts = [];
@@ -64,6 +72,42 @@ const Time = () => {
     return totalContextTime;
   };
 
+  // Function to send new interval time: same logic as adding context:
+  const sendEndTime = (id, time, index) => {
+    const newTasksArray = [];
+    // Iterate tasks, find match, add the new time
+    tasks.forEach(task => {
+      if (task.id === id) {
+        // edited date:
+        let date = parseJSON(task.timerEnds[index]);
+        date.setHours(time.slice(0, 2), time.slice(3, 5), 0);
+        task.timerEnds[index] = date;
+        // Update this specific task in db: sends new timer event
+        taskService.updateById(id, task);
+      }
+      newTasksArray.push(task);
+    });
+    setTasks(newTasksArray);
+  };
+
+  // Function to send new interval time: same logic as adding context:
+  const sendStartTime = (id, time, index) => {
+    const newTasksArray = [];
+    // Iterate tasks, find match, add the new time
+    tasks.forEach(task => {
+      if (task.id === id) {
+        // edited date:
+        let date = parseJSON(task.timerStarts[index]);
+        date.setHours(time.slice(0, 2), time.slice(3, 5), 0);
+        task.timerStarts[index] = date;
+        // Update this specific task in db: sends new timer event
+        taskService.updateById(id, task);
+      }
+      newTasksArray.push(task);
+    });
+    setTasks(newTasksArray);
+  };
+
   return (
     <div>
       <h3>Time</h3>
@@ -77,17 +121,88 @@ const Time = () => {
       <br />
       <ol>
         <h4>Time active between start and end time for each task:</h4>
-        {tasks.map(
-          task =>
-            calculateActiveTime(task) > 0 && (
-              <li key={task.id}>
-                {task.name}
-                <br />
-                Time active: {calculateActiveTime(task)} min
-                <br />
-              </li>
-            )
-        )}
+        {tasks.map((task, index) => (
+          <li key={task.id}>
+            {task.name}
+            <br />
+            Time active:{" "}
+            {calculateActiveTime(task) > 60
+              ? (calculateActiveTime(task) / 60).toFixed(0) + " h"
+              : calculateActiveTime(task) + " min"}
+            <br />
+            {showIntervalIndex !== index && (
+              <button onClick={() => setShowIntervalIndex(index)}>
+                Show intervals
+              </button>
+            )}
+            {showIntervalIndex === index && (
+              <button onClick={() => setShowIntervalIndex("")}>
+                Hide intervals
+              </button>
+            )}
+            {/* Listing intervals: apply filters too: 
+                    Complicated code here: apply filters, count differences
+                    using date-fns here.
+                */}
+            {showIntervalIndex === index &&
+              task.timerStarts.map(
+                (timerStart, index) =>
+                  compareAsc(parseJSON(timerStart), startTime) === 1 &&
+                  compareDesc(parseJSON(timerStart), endTime) !== -1 && (
+                    <div key={index}>
+                      <br />
+                      <strong>task interval</strong>
+                      <br />
+                      {new Date(timerStart).toString().slice(0, 24)}
+                      <br />
+                      - edit start time:
+                      <TimePickerButton
+                        time={timerStart}
+                        index={index}
+                        id={task.id}
+                        sendStartTime={sendStartTime}
+                        sendEndTime={sendEndTime}
+                      />
+                      {task.timerEnds[index]
+                        ? new Date(task.timerEnds[index])
+                            .toString()
+                            .slice(0, 24)
+                        : "active now"}
+                      <br />
+                      - edit end time:
+                      <TimePickerButton
+                        time={task.timerEnds[index]}
+                        isEndTime={true}
+                        index={index}
+                        id={task.id}
+                        sendStartTime={sendStartTime}
+                        sendEndTime={sendEndTime}
+                      />
+                      {task.timerEnds[index]
+                        ? differenceInMinutes(
+                            parseJSON(task.timerEnds[index]),
+                            parseJSON(timerStart)
+                          ) > 60
+                          ? (
+                              differenceInMinutes(
+                                parseJSON(task.timerEnds[index]),
+                                parseJSON(timerStart)
+                              ) / 60
+                            ).toFixed(0) + "h"
+                          : differenceInMinutes(
+                              parseJSON(task.timerEnds[index]),
+                              parseJSON(timerStart)
+                            ) + "min"
+                        : differenceInMinutes(
+                            parseJSON(parseJSON(new Date())),
+                            parseJSON(timerStart)
+                          )}
+                    </div>
+                  )
+              )}
+            <br />
+          </li>
+        ))}
       </ol>
       <ol>
         <h4>Time active between start and end time for each context:</h4>
@@ -98,7 +213,10 @@ const Time = () => {
               <li key={context}>
                 {context}
                 <br />
-                Time active: {calculateTimeForContext(context)} min
+                Time active:{" "}
+                {calculateTimeForContext(context) > 60
+                  ? (calculateTimeForContext(context) / 60).toFixed(0) + " h"
+                  : calculateTimeForContext(context) + " min"}
                 <br />
               </li>
             )
