6ce00703e96267223305e491cd3d77417a9051f7
Andreas Achte <andreas.achte@gmail.com>
2022-12-18 14:00:41 +0200

    Ui component: user can edit start and end times for each task.

---
+++ src/components/TimePickerButton.js
@@ -0,0 +1,31 @@
+import TimePicker from "react-time-picker";
+import { useState } from "react";
+import { parseJSON } from "date-fns";
+
+const TimePickerButton = props => {
+  const [value, onChange] = useState("00:00");
+  const index = props.index;
+  const id = props.id;
+
+  if (props.isEndTime) {
+    return (
+      <div>
+        <TimePicker locale="fi" onChange={onChange} value={value} />
+        <button onClick={() => props.sendEndTime(id, value, index)}>
+          send
+        </button>
+      </div>
+    );
+  } else {
+    return (
+      <div>
+        <TimePicker locale="fi" onChange={onChange} value={value} />
+        <button onClick={() => props.sendStartTime(id, value, index)}>
+          send
+        </button>
+      </div>
+    );
+  }
+};
+
+export default TimePickerButton;
